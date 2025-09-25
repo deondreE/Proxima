6a75b6f10da478c5454f9d54df267dc152a83952
deondre <deondre.eldest.english@gmail.com>
2025-09-25 11:28:23 -0500

    added global header called Proxima.h for imports

---
+++ include/Proxima.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Core/EventDispatcher.hpp" 
+#include "Core/ProximaEvent.hpp"
+
+#include "UI/Button.hpp"
+#include "UI/ConfigManager.hpp"
+#include "UI/Image.hpp"
+#include "UI/Line.hpp"
+#include "UI/Rect.hpp"  
+#include "UI/Slider.hpp"  
+#include "UI/View.hpp"
+#include "UI/TextInput.hpp"
+#include "UI/Text.hpp"
+#include "UI/StackLayout.hpp"
+#include "UI/Window.hpp"
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <format>
+#include <iostream>
+#include <memory>
